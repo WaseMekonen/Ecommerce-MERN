48929b5b1e11fe5e824f90dffa9e39b9ca754879
Wase Mekonen <wase@Wases-MacBook-Air.local>
2022-12-08 23:55:41 +0200

    fetching data from express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>welcome to Big electronic</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
